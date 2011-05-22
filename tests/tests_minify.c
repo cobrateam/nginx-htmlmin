8399177efb711213883a9c2a28d977b74195e94a
Francisco Souza <francisco@franciscosouza.net>
2011-05-22 02:38:38 -0300

    Free :)

--- tests/tests_minify.c
+++ tests/tests_minify.c
@@ -10,6 +10,7 @@ void testMinifySimpleHtml()
     char *string = (char *)malloc(1000 * sizeof(char));
     strcpy(string, "          <p>Hello</p>");
     assert(strcmp(minify(string), "<p>Hello</p>") == 0);
+    free(string);
 }
 
 void testMinifyHtmlWithSpace()
@@ -17,6 +18,7 @@ void testMinifyHtmlWithSpace()
     char *string = (char *)malloc(1000 * sizeof(char));
     strcpy(string, "          <p>Hello world</p>");
     assert(strcmp(minify(string), "<p>Hello world</p>") == 0);
+    free(string);
 }
 
 void testMinifyMultilineHtml()
@@ -24,4 +26,5 @@ void testMinifyMultilineHtml()
     char *string = (char *)malloc(1000 * sizeof(char));
     strcpy(string, "<html>\n<body>\n    <p>Hello world!</p>\n    </body>\n</html>");
     assert(strcmp(minify(string), "<html><body><p>Hello world!</p></body></html>") == 0);
+    free(string);
 }
